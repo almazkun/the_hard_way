9e0200f0153cd25635c067737c398aa8c5f7d51a
Almaz Kunpeissov <almaz.kun@gmail.com>
2019-08-21 15:02:17 +0900

    ex11

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print("How old are you?")
+age = input()
+print("What is your height?")
+height = input()
+print("What is your weight?")
+weight = input()
+
+print("So, you are {}, your weight is {} and you are {} tall".format(age, weight, height))

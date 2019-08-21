3f0b2b2a840b51ec7bcd659e6ee0ec6493f3bcbb
Almaz Kunpeissov <almaz.kun@gmail.com>
2019-08-21 14:37:49 +0900

    ex9

---
+++ ex9.py
@@ -0,0 +1,12 @@
+days = "Mn Th We Tu Fr St Su"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+print("This days: {}".format(days))
+print("Months are: {}".format(months))
+print("""
+Many 
+words 
+were 
+written 
+here
+      """)

6952be7ce214beb58c8807b9f388a3db99f57e80
Almaz Kunpeissov <almaz.kun@gmail.com>
2019-08-21 15:40:28 +0900

    ex12

---
+++ ex12.py
@@ -0,0 +1,5 @@
+age = input("How old are you: ")
+height = input("How tall are you: ")
+weight = input("How heavy are you: ")
+
+print("So, you are {} years old, {} tall and as heavy as {}".format(age, height, weight))

e061b6621a9ac8b1e6b9a9c2e7c3af63cf34cc49
Almaz Kunpeissov <almaz.kun@gmail.com>
2019-08-21 14:22:03 +0900

    ex7

---
+++ ex7.py
@@ -0,0 +1,14 @@
+print("Mary had a little {}.".format("lam"))
+print("Its fure were white as {}".format("snow"))
+print("its always followed her")
+print("\\(X_x)/ " * 10)
+
+e1 = "B"
+e2 = "a"
+e3 = "d"
+e4 = "g"
+e5 = "u"
+e6 = "y"
+
+print(e1 + e2 + e3),
+print(e4 + e5 + e6)

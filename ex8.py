73d6129abdd61336dfd185a26e51713655c19ed3
Almaz Kunpeissov <almaz.kun@gmail.com>
2019-08-21 14:30:08 +0900

    ex8

---
+++ ex8.py
@@ -0,0 +1,6 @@
+formater = "{} {} {} {}"
+
+print(formater.format(1, 2, 3, 4))
+print(formater.format("one", "two", "three", "four"))
+print(formater.format(formater, formater, formater, formater))
+print(formater.format("sleeping,", "pony", "only", "boy!"))

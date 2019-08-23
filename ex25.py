e8b6dbe7aee2c9593ff9079968bc0511db665abc
Almaz Kunpeissov <almaz.kun@gmail.com>
2019-08-23 15:23:33 +0900

    ex25

---
+++ ex25.py
@@ -0,0 +1,33 @@
+def break_words(stuff):
+    """This function divide sentences into words"""
+    words = stuff.split(" ")
+    return words
+
+
+def sort_words(words):
+    """Sort words"""
+    return sorted(words)
+
+
+def print_first_word(words):
+    """Prints fist word"""
+    word = words.pop(0)
+    print(word)
+
+
+def print_last_word(words):
+    """Prints last word"""
+    word = words.pop(-1)
+    print(word)
+
+
+def sort_sentence(sentence):
+    """Return sorted sentence"""
+    words = break_words(sentence)
+    return sort_words(words)
+
+
+def print_first_and_last_words(sentence):
+    words = break_words(sentence)
+    print_first_word(words)
+    print_last_word(words)

dc6c0f776b806150cf83207c4e7355ef2054c868
Almaz Kunpeissov <almaz.kun@gmail.com>
2019-08-23 16:15:56 +0900

    ex26

--- ex25.py
+++ ex25.py
@@ -31,3 +31,9 @@ def print_first_and_last_words(sentence):
     words = break_words(sentence)
     print_first_word(words)
     print_last_word(words)
+
+
+def print_first_and_last_sorted(sentence):
+    words = sort_sentence(sentence)
+    print_first_word(words)
+    print_last_word(words)
---
+++ ex26.py
@@ -0,0 +1,61 @@
+import ex25
+
+
+print("Давайте попрактикуемся!")
+print("Вы должны знать об управляющих последовательностях с символом \\, которые \n управляют переносом строк и \t отступами.")
+
+
+poem = """
+\tДля счастья
+мне совсем немного надо.
+Хочу тебя \n я нежно обнимать,
+Хочу всегда
+я быть с тобою рядом
+\n\t\tИ никогда не отпускать!
+"""
+
+
+print("--------------")
+print(poem)
+print("--------------")
+
+
+five = 10 - 2 + 3 - 6
+print("Здесь должна быть пятерка: {}".format(five))
+
+def secret_formula(started):
+    jelly_beans = started * 500
+    jars = jelly_beans / 1000
+    crates = jars / 100
+    return jelly_beans, jars, crates
+
+
+start_point = 10000
+beans, jars, crates = secret_formula(start_point)
+
+
+print("Начиная с: {}".format(start_point))
+print("У нас есть {} бобров, {} банок и {} ящиков.".format(beans, jars, crates))
+
+
+start_point = start_point / 10
+
+
+print("Также можно поступить следующим образом:")
+print("У нас есть %d бобов, %d банок и %d коробков." % secret_formula(start_point))
+
+
+sentence = "All good things come to those who weight."
+
+
+words = ex25.break_words(sentence)
+sorted_words = ex25.sort_words(words)
+
+ex25.print_first_word(words)
+ex25.print_last_word(words)
+ex25.print_first_word(sorted_words)
+ex25.print_last_word(sorted_words)
+sorted_words = ex25.sort_sentence(sentence)
+print(sorted_words)
+ex25.print_first_and_last_words(sentence)
+ex25.print_first_and_last_sorted(sentence)

181c6f57a3f22f8fc9b4403ed00f97acd7ed78a0
Almaz Kunpeissov <almaz.kun@gmail.com>
2019-08-26 10:56:12 +0900

    ex29

---
+++ ex29.py
@@ -0,0 +1,35 @@
+people = 20
+cats = 30
+dogs = 15
+
+
+if people < cats:
+    print("Too many Cats!")
+
+
+if people > cats:
+    print("Too mane people! We are doomed!")
+
+
+if people < dogs:
+    print("Now so doomed")
+
+
+if people > dogs:
+    print("world is dry")
+
+
+dogs += 5
+
+
+if people >= dogs:
+    print("We eill be fine")
+
+
+if people <= dogs:
+    print("less or equle to dogs")
+
+
+if people == dogs:
+    print("Equal")
+    

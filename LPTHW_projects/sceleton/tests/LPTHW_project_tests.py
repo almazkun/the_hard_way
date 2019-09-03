b0c47b259fe3c268cfa84360ce020d718f5b6393
Almaz Kunpeissov <almaz.kun@gmail.com>
2019-09-03 15:28:45 +0900

    ex46

---
+++ LPTHW_projects/sceleton/tests/LPTHW_project_tests.py
@@ -0,0 +1,14 @@
+from nose.tools import *
+import LPTHW_project
+
+
+def setup():
+    print("Installing...")
+
+
+def teardown():
+    print("Compliting...")
+
+
+def test_basic():
+    print("EXECUTING!")

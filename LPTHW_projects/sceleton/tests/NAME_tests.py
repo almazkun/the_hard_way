3192a5414ae6572e303d8e3a70ec0f6e5e3acc37
Almaz Kunpeissov <almaz.kun@gmail.com>
2019-09-03 16:46:10 +0900

    ex47 WIT

---
+++ LPTHW_projects/sceleton/tests/NAME_tests.py
@@ -0,0 +1,14 @@
+from nose.tools import *
+import NAME_project
+
+
+def setup():
+    print("Installing...")
+
+
+def teardown():
+    print("Compliting...")
+
+
+def test_basic():
+    print("EXECUTING!")
